9dd56d5dd2a8a8783d160c68eb8474d04723669d
Ash <ashot.muradyan16@gmail.com>
2022-05-25 23:01:10 +0300

    first

---
+++ src/components/Routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+
+import { Results } from './Results'
+
+export const Routes = () => {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/search" />
+        </Route>
+        <Route exact path={["/search", "/image", "/news", "/videos"]}>
+          <Results />
+        </Route>
+      </Switch>
+    </div>
+  )
+}
---
+++ src/components/Search.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import { useDebounce } from 'use-debounce/lib'
+
+import { useResultContext } from '../contexts/ResultContextProvider'
+
+import { Links } from './Links'
+
+export const Search = () => {
+  
+  const [text, setText] = useState('Elon Musk')
+  const { setSearchTerm } = useResultContext()
+  const [debouncedValue] = useDebounce(text, 300)
+
+  useEffect(() => {
+    if(debouncedValue) setSearchTerm(debouncedValue)
+  }, [debouncedValue])
+
+  return (
+    <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
+      <input 
+        value={text}
+        type="text"
+        className="sm:w-96 w-80 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
+        placeholder="Search it or type URL"
+        onChange={(e) => setText(e.target.value)}
+      />
+      {!text && (
+        <button type="button" className="absolute top-1.5 right-4 text-2xl text-gray-500" onClick={() => setText("")}>
+
+        </button>
+      )}
+      <Links />
+    </div>
+  )
+}
